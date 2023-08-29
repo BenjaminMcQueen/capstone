8fe4bf7d9edbf9afdaf7b655ea90dcf3432b47cd
chudaa203 <chudaa203@gmail.com>
2023-08-29 16:45:43 -0400

    first day

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import Attractions from "./components/Attractions";
+import Map from "./components/Map";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <Router>
+      <div className="App">
+        <Header />
         <Routes>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/attractions" element={<Attractions />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </Router>
   );
 }
 

655fdb181197846b89ccf22210fb8bc4d7c1d2e5
BenaSPACE <BenaSPACE@gmail.com>
2023-08-30 11:28:43 -0400

    got the database and .env stuff working :)

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,5 +15,5 @@ app.use(cors());
 const dbURI = process.env.URI;
 console.log(dbURI || "URI undefined for some reason");
 mongoose.connect(dbURI)
-  .then((result) => {app.listen(3000); console.log("Listening on port 3000");})
+  .then((result) => {app.listen(port); console.log("Listening on port", port);})
   .catch((err) => console.log(err));

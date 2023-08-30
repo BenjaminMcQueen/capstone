ea99d478fa4c7e33afe466542047034d6aa068a5
BenaSPACE <BenaSPACE@gmail.com>
2023-08-30 11:24:10 -0400

    trying to fix .env files

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,6 @@ app.use(express.json);
 app.use(morgan('dev'));
 
 const URI = process.env.URI
-// mongoose.connect
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => {app.listen(3000); console.log("Listening on port 3000");})
+  .catch((err) => console.log(err));

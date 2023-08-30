43724e5de0a143f02a3af29b5fc0d68250cf97eb
BenaSPACE <BenaSPACE@gmail.com>
2023-08-30 11:24:36 -0400

    .env bullhockey

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json);
 app.use(morgan('dev'));
+app.use(cors());
 
-const URI = process.env.URI
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURI = process.env.URI;
+console.log(dbURI || "URI undefined for some reason");
+mongoose.connect(dbURI)
   .then((result) => {app.listen(3000); console.log("Listening on port 3000");})
   .catch((err) => console.log(err));

271d1406721ca426f76c12ffd340c0cccc89cf3f
BenaSPACE <BenaSPACE@gmail.com>
2023-08-31 16:08:06 -0400

    finished attraction model, started error logger

--- backend/routes/attractions.js
+++ backend/routes/attractions.js
@@ -3,7 +3,9 @@ const attractionController = require('../controllers/Attractions.controller');
 
 
 // router.get('/attraction', )
+router.route('/:id').get(attractionController.get)
 router.route('/').post(attractionController.post)
+router.route('/:id').post(attractionController.update)
 router.route('/:id').delete(attractionController.delete)
 
 module.exports = router;

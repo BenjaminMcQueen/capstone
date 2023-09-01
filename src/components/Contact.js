631a8b9a6f815a2fd40b0397261c52ee5d65d8b5
chudaa203 <chudaa203@gmail.com>
2023-09-01 17:03:58 -0400

    third day

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,34 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h1>Contact us</h1>
-      <form>
-        <label>FullName: </label>
-        <input type="text" required></input>
-        <label>Email:</label>
-        <input type="email" required></input>
-        <label>Topics</label>
-        <select>
-          <option value="complaints">Complaints</option>
-          <option value="feedbacks">Feedbacks</option>
-        </select>
-        <button>Submit</button>
-      </form>
+    <h1 className="contact-heading create">Contact us</h1>
+      <div className="form-container">
+        <form>
+          <div>
+            <label>FullName: </label>
+            <input type="text" required></input>
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" required></input>
+          </div>
+          <div>
+            <label>Topics</label>
+            <select>
+              <option value="general"> General </option>
+              <option value="complaints"> Complaints </option>
+              <option value="feedbacks"> Feedbacks </option>
+            </select>
+          </div>
+          <div>
+            <label>Message:</label>
+            <textarea></textarea>
+          </div>
+          <div>
+            <button className="button">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }

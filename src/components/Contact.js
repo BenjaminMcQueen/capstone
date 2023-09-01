f7b6b092bf16fabbd43d69072e1ad5cae2ecd8b8
chudaa203 <chudaa203@gmail.com>
2023-09-01 15:33:02 -0400

    third day

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function Contact() {
-  return 
-  <div>
-    Contact Page
-  </div>;
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <form>
+        <label>FullName: </label>
+        <input type="text" required></input>
+        <label>Email:</label>
+        <input type="email" required></input>
+        <label>Topics</label>
+        <select>
+          <option value="complaints">Complaints</option>
+          <option value="feedbacks">Feedbacks</option>
+        </select>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;

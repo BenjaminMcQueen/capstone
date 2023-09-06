d722e0f53e1f6cb69179b615f5ed217a5520d6bc
chudaa203 <chudaa203@gmail.com>
2023-09-06 16:01:17 -0400

    6th day updates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import L from "leaflet";
+// import markerIconPng from 'leaflet/dist/images/marker-icon.png'
+import "leaflet-defaulticon-compatibility";
 
 const Map = () => {
   useEffect(() => {
-    // initializing the map
-    const map = L.map("map").setView([39.1031, -84.512], 13);
+    //declearing and retriving the DOM element with id "map" using leaflet's utility function
+    var container = L.DomUtil.get("map");
 
+    //checking if the container element exisits
+    if (container != null) {
+      // setting id of container to null so it can detach from any existing map instances
+      container._leaflet_id = null;
+    }
+    // initializing the map
+    var map = L.map("map").setView([39.1031, -84.512], 13);
     // Adding a title to the map views
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
@@ -16,7 +26,6 @@ const Map = () => {
     // Adding markers to each of attractions using L (leaflet JS liberary)
     L.marker([39.1427, -84.5089]).addTo(map).bindPopup("Cincinnati Zoo"); //cincinnati zoo
     L.marker([39.1304, -84.5149]).addTo(map).bindPopup("Museum"); // Cincinnati Museum
-    L.marker([39.0971, -84.5082]).addTo(map).bindPopup("Baseball Game Stadium"); // Baseball Game Stadium
     L.marker([39.094, -84.5255]).addTo(map).bindPopup("Ohio Riverfront"); // Ohio Riverfront
     L.marker([39.1112, -84.5175]).addTo(map).bindPopup("Over-the-Rhine"); // Over-the-Rhine
     L.marker([39.0986, -84.5079]).addTo(map).bindPopup("Aquarium"); // Aquarium
@@ -26,12 +35,8 @@ const Map = () => {
     L.marker([39.104, -84.5123]).addTo(map).bindPopup("Shopping"); // Shopping
     L.marker([39.1055, -84.5127]).addTo(map).bindPopup("Hiking"); // Hiking
     L.marker([39.0925, -84.5087]).addTo(map).bindPopup("Fishing"); // Fishing
+    L.marker([39.0971, -84.5082]).addTo(map).bindPopup("Baseball Stadium"); // Baseball Stadium
   }, []); //empty array run this effect only once
-
-  // Cleanup: remove the map instance when the component unmounts
-  // return () => {
-  //   map.remove();
-  // };
 
   return <div id="map" style={{ height: "100vh" }}></div>;
 };
